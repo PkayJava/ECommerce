7478997d8a1de8dd296da4ab382f675433a162a6
Socheat KHAUV <pkayjava@gmail.com>
2017-02-05 14:06:41 +0800

    udropship error

--- backoffice/src/main/webapp/WEB-INF/db/migration/mysql__platform_localization_insert.sql
+++ backoffice/src/main/webapp/WEB-INF/db/migration/mysql__platform_localization_insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO platform_localization (platform_localization_id, `key`, language, label, version)
 VALUES
-  (uuid_short(), 'null', '', 'Select One', 1),
-  (uuid_short(), 'label.login', '', 'Login', 1),
-  (uuid_short(), 'label.password', '', 'Password', 1),
-  (uuid_short(), 'button.login', '', 'Login', 1),
-  (uuid_short(), 'required', '', ' is required.', 1),
-  (uuid_short(), 'incorrect', '', ' is incorrect.', 1),
-  (uuid_short(), 'brand.text.mini', '', ' Shop', 1),
-  (uuid_short(), 'brand.text', '', ' ShopKH', 1),
-  (uuid_short(), 'nullValid', '', 'Select One', 1);
+  (1, 'null', '', 'Select One', 1),
+  (2, 'label.login', '', 'Login', 1),
+  (3, 'label.password', '', 'Password', 1),
+  (4, 'button.login', '', 'Login', 1),
+  (5, 'required', '', ' is required.', 1),
+  (6, 'incorrect', '', ' is incorrect.', 1),
+  (7, 'brand.text.mini', '', ' Shop', 1),
+  (7, 'brand.text', '', ' ShopKH', 1),
+  (8, 'nullValid', '', 'Select One', 1);

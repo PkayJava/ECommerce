fba2ecd555f0421780bcbc7ac9c779880eddcff9
Socheat KHAUV <pkayjava@gmail.com>
2017-02-17 22:11:36 +0800

    allow null value

--- backoffice/src/main/webapp/WEB-INF/db/migration/mysql__platform_setting_ddl.sql
+++ backoffice/src/main/webapp/WEB-INF/db/migration/mysql__platform_setting_ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE `platform_setting` (
   `platform_setting_id` BIGINT(19)   NOT NULL,
   `key`                 VARCHAR(100) NOT NULL,
   `description`         VARCHAR(255),
-  `value`               VARCHAR(255) NOT NULL,
+  `value`               VARCHAR(255),
   `version`             INT(11)      NOT NULL DEFAULT 1,
 
   KEY (`version`),

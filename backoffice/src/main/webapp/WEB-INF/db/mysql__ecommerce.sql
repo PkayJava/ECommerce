5a69ef6ae8590357281be0cc2b1ff67f0fdb1c07
Socheat KHAUV <pkayjava@gmail.com>
2017-02-20 21:48:52 +0800

    update text

--- backoffice/src/main/webapp/WEB-INF/db/mysql__ecommerce.sql
+++ backoffice/src/main/webapp/WEB-INF/db/mysql__ecommerce.sql
@@ -362,15 +362,14 @@ CREATE TABLE `ecommerce_order_item` (
 -- ################################################################
 CREATE TABLE `ecommerce_page` (
 
-  `ecommerce_page_id` BIGINT(19)    NOT NULL,
-  `title`             VARCHAR(255)  NOT NULL,
-  `text`              VARCHAR(1000) NOT NULL,
+  `ecommerce_page_id` BIGINT(19)   NOT NULL,
+  `title`             VARCHAR(255) NOT NULL,
+  `text`              TEXT         NOT NULL,
   `order`             INT(11),
   `enabled`           BIT(1),
 
   KEY (`order`),
   KEY (`enabled`),
-  KEY (`text`),
   UNIQUE KEY (`title`),
   PRIMARY KEY (`ecommerce_page_id`)
 );
